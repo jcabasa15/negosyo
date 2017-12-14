eec8c10bd18a61caabd4b48d8fb845f698d74129
jcabasa15 <34529581+jcabasa15@users.noreply.github.com>
2017-12-14 11:26:53 +0800

    Add files via upload

---
+++ anproduct.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nego$yo</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Bootstrap core CSS-->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Page level plugin CSS-->
+    <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin.css" rel="stylesheet">
+</head>
+<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+  <!--Navbar-->
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top" id="mainNav">
+      <a class="navbar-brand pl-4" href="index.html"><img src="img/nego$yo.png" class="d-inline-block alt="Logo" style="width:50px;"> NEGO$YO</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <!--Sidebar-->
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav navbar-sidenav pt-3" style="margin-top: 60px;" id="exampleAccordion">
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+            <a class="nav-link" href="index.html">
+              <i class="fa fa-fw fa-dashboard"></i>
+              <span class="nav-link-text"> Home</span>
+            </a>
+          </li>
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Catalog">
+                    <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseCatalog" data-parent="#exampleAccordion">
+                        <i class="fa fa-fw fa-book"></i>
+                        <span class="nav-link-text"> Catalog</span>                        
+                    </a>
+                    <ul class="sidenav-second-level collapse" id="collapseCatalog">
+                            <li>
+                                <a href="products.php">Products</a>
+                            </li>
+                            <li>
+                                <a href="categories.php">Categories</a>
+                            </li>
+                        </ul>
+                </li>
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Customers">
+            <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
+              <i class="fa fa-fw fa-user-circle"></i>
+              <span class="nav-link-text"> Customers</span>
+            </a>
+                    <ul class="sidenav-second-level collapse" id="collapseComponents">
+                        <li>
+                            <a href="navbar.html">Customers</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Customer Roles</a>
+                        </li>
+                        <li>
+                            <a href="navbar.html">Online Customers</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Customer Reports</a>
+                        </li>
+                        <li>
+                            <a href="navbar.html">Vendors</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Activity Log</a>
+                        </li>
+                    </ul>
+          </li>
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Sales">
+            <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseSales" data-parent="#exampleAccordion">
+              <i class="fa fa-fw fa-shopping-cart"></i>
+              <span class="nav-link-text"> Sales</span>
+            </a>
+                    <ul class="sidenav-second-level collapse" id="collapseSales">
+                        <li>
+                            <a href="navbar.html">Orders</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Shipments</a>
+                        </li>
+                        <li>
+                            <a href="navbar.html">Return Requests</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Recurring Payments</a>
+                        </li>
+                        <li>
+                            <a href="navbar.html">Gift Cards</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Currents Shopping Cards</a>
+                        </li>
+                         <li>
+                            <a href="navbar.html">Currents Wishlists</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Best Sellers</a>
+                        </li>
+                        <li>
+                            <a href="navbar.html">Products Never Purchased</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Country Report</a>
+                        </li>
+                    </ul>
+          </li>
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
+                    <a class="nav-link" href="table.php">
+                        <i class="fa fa-fw fa-truck"></i>
+                        <span class="nav-link-text"> New Arrivals</span>
+                    </a>
+               </li>
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
+            <a class="nav-link" href="table.php">
+              <i class="fa fa-fw fa-table"></i>
+              <span class="nav-link-text"> Tables</span>
+            </a>
+          </li>
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">
+            <a class="nav-link" href="#">
+              <i class="fa fa-fw fa-area-chart"></i>
+              <span class="nav-link-text"> Chart</span>
+            </a>
+          </li>
+        </ul>
+        <ul class="navbar-nav sidenav-toggler">
+            <li class="nav-item">
+                <a class="nav-link text-center" id="sidenavToggler">
+                  <i class="fa fa-fw fa-angle-left"></i>
+                </a>
+            </li>
+        </ul>
+        <!--Navbar-->
+        <ul class="navbar-nav ml-auto">
+        <form class="form-inline my-2 my-lg-0 mr-lg-2">
+              <div class="input-group">
+               <input class="form-control" type="text" placeholder="Search for...">
+                  <span class="input-group-btn">
+                    <button class="btn btn-success" type="button">
+                      <i class="fa fa-search"> </i>
+                    </button>
+                  </span>
+              </div>
+        </form>
+        <li class="nav-item">
+              <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+              <i class="fa fa-fw fa-sign-out"></i>Logout
+              </a>
+          </li>
+      </ul>
+      </div>
+  </nav>
+  <div class="content-wrapper">
+      <div class="container-fluid pt-3">
+      <!--BreadCrumbs-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="#">Dashboard</a>
+          </li>
+          <li class="breadcrumb-item active">Products</li>
+        </ol>
+      </div>
+      <!--Add new Categories-->
+      <form method='post' action='' enctype="multipart/form-data">
+        <div class="container">
+        <h5 class="pull-left">
+            Add New Product &nbsp&nbsp
+            <small>
+            <a href="products.php" class="product">
+              <i class="fa fa-arrow-circle-left"></i>
+              <span>Go back to the Products List</span>
+            </a>
+          </small>
+        </h5>
+        <div class="pull-right">
+          <button class="btn btn-primary btn-sm ml-3 mt-3.5" onclick="location.href='#'">Save</button>
+          <button class="btn btn-primary btn-sm ml-3 mt-3.5" onclick="location.href='#'">Save and Continue Edit</button>
+        </div>
+      </div>
+        <div class="container col-md-6">  
+        <div class="form-group">
+          <br/>
+          <br/>
+          <label for="exampleInputName1">Product Name</label>
+          <input type="textarea" class="form-control" id="exampleInputName1"  placeholder="Enter Product Name">
+          <small class="form-text text-muted">Please Enter your new Product here</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputCategory1">Category</label>
+            <select class="form-control valid" data-val="true" data-val-required="The Category field is required." id="SearchCategoryId" name="SearchCategoryId">
+                <option selected="selected" value="0">All</option>
+                <option value="1">Computers</option>
+                <option value="2">Clothes</option>
+            </select>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputeditor1">Description</label>
+          <textarea name="editor1"></textarea>
+        </div>
+        <div class="form-group">
+           <label for="exampleInputPrice1">Price</label>
+           <input type="text" data-val="true" data-val-required="The field Price must be a number." id="Price" name="Price" value="0.00" class="form-control valid" style="display: block">
+        </div>
+        <div class="form-group">
+            <label for="producttag">Product Tag</label>
+            <input type="text" data-val="true" id="ptag" name="ptag" class="form-control valid" placeholder="Enter your best Product Tag">
+        </div>
+        <div class="form-group">
+            <label for="qty">Quantity</label>
+            <input type="text" data-val="true" data-val-required="Numbers Only" id="qty" name="qty" class="form-control valid" placeholder="Quantity of your Product">
+        </div>
+         <div form-group>
+            <label for="picture">Image</label>
+            <input type="file" name="image" value="Upload">
+        </div>
+      </div>
+      </form>
+  </div>
+  <!-- Logout Modal-->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+            <button type="button" class="btn btn-primary">Logout</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="sticky-footer">
+      <div class="container">
+        <div class="text-center">
+          <small>COPYRIGHT © 2017 NEGO$YO PHILIPPINES. ALL RIGHT RESERVED</small>
+        </div>
+      </div>
+    </footer>
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Page level plugin JavaScript-->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+    <script src="vendor/datatables/jquery.dataTables.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin.min.js"></script>
+    <!-- Custom scripts for this page-->
+    <script src="js/sb-admin-datatables.min.js"></script>
+    <script src="js/sb-admin-charts.min.js"></script>
+    <script src="//cdn.ckeditor.com/4.7.3/standard/ckeditor.js"></script>
+    <script>
+
+      CKEDITOR.replace('editor1');
+
+    </script> 
+</body>
+</html>
---
+++ products.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nego$yo</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Bootstrap core CSS-->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Page level plugin CSS-->
+    <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin.css" rel="stylesheet">
+</head>
+<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+  <!--Navbar-->
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top" id="mainNav">
+      <a class="navbar-brand pl-4" href="index.html"><img src="img/nego$yo.png" class="d-inline-block alt="Logo" style="width:50px;"> NEGO$YO</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <!--Sidebar-->
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav navbar-sidenav pt-3" style="margin-top: 60px;" id="exampleAccordion">
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+            <a class="nav-link" href="index.html">
+              <i class="fa fa-fw fa-dashboard"></i>
+              <span class="nav-link-text"> Home</span>
+            </a>
+          </li>
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Catalog">
+                    <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseCatalog" data-parent="#exampleAccordion">
+                        <i class="fa fa-fw fa-book"></i>
+                        <span class="nav-link-text"> Catalog</span>                        
+                    </a>
+                    <ul class="sidenav-second-level collapse" id="collapseCatalog">
+                            <li>
+                                <a href="products.php">Products</a>
+                            </li>
+                            <li>
+                                <a href="categories.php">Categories</a>
+                            </li>
+                    </ul>
+                </li>
+         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Customers">
+            <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
+              <i class="fa fa-fw fa-user-circle"></i>
+              <span class="nav-link-text"> Customers</span>
+            </a>
+                    <ul class="sidenav-second-level collapse" id="collapseComponents">
+                        <li>
+                            <a href="navbar.html">Customers</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Customer Roles</a>
+                        </li>
+                        <li>
+                            <a href="navbar.html">Online Customers</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Customer Reports</a>
+                        </li>
+                        <li>
+                            <a href="navbar.html">Vendors</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Activity Log</a>
+                        </li>
+                    </ul>
+          </li>
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Sales">
+            <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseSales" data-parent="#exampleAccordion">
+              <i class="fa fa-fw fa-shopping-cart"></i>
+              <span class="nav-link-text"> Sales</span>
+            </a>
+                    <ul class="sidenav-second-level collapse" id="collapseSales">
+                        <li>
+                            <a href="navbar.html">Orders</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Shipments</a>
+                        </li>
+                        <li>
+                            <a href="navbar.html">Return Requests</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Recurring Payments</a>
+                        </li>
+                        <li>
+                            <a href="navbar.html">Gift Cards</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Currents Shopping Cards</a>
+                        </li>
+                         <li>
+                            <a href="navbar.html">Currents Wishlists</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Best Sellers</a>
+                        </li>
+                        <li>
+                            <a href="navbar.html">Products Never Purchased</a>
+                        </li>
+                        <li>
+                            <a href="cards.html">Country Report</a>
+                        </li>
+                    </ul>
+          </li>
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
+                    <a class="nav-link" href="table.php">
+                        <i class="fa fa-fw fa-truck"></i>
+                        <span class="nav-link-text"> New Arrivals</span>
+                    </a>
+               </li>
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
+            <a class="nav-link" href="table.php">
+              <i class="fa fa-fw fa-table"></i>
+              <span class="nav-link-text"> Tables</span>
+            </a>
+          </li>
+          <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">
+            <a class="nav-link" href="#">
+              <i class="fa fa-fw fa-area-chart"></i>
+              <span class="nav-link-text"> Chart</span>
+            </a>
+          </li>
+        </ul>
+        <ul class="navbar-nav sidenav-toggler">
+            <li class="nav-item">
+                <a class="nav-link text-center" id="sidenavToggler">
+                  <i class="fa fa-fw fa-angle-left"></i>
+                </a>
+            </li>
+        </ul>
+        <!--Navbar-->
+        <ul class="navbar-nav ml-auto">
+        <form class="form-inline my-2 my-lg-0 mr-lg-2">
+              <div class="input-group">
+               <input class="form-control" type="text" placeholder="Search for...">
+                  <span class="input-group-btn">
+                    <button class="btn btn-success" type="button">
+                      <i class="fa fa-search"> </i>
+                    </button>
+                  </span>
+              </div>
+        </form>
+        <li class="nav-item">
+              <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+              <i class="fa fa-fw fa-sign-out"></i>Logout
+              </a>
+          </li>
+      </ul>
+      </div>
+  </nav>
+  <div class="content-wrapper">
+      <div class="container-fluid pt-3">
+      <!--BreadCrumbs-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="#">Dashboard</a>
+          </li>
+          <li class="breadcrumb-item active">Products</li>
+        </ol>
+        <!--Manage Products-->
+    <div class="card mb-3">
+        <div class="card-header">
+          <i class="fa fa-table"></i>&nbsp Products</div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+              <thead>
+                <tr>
+                  <th>Product ID</th>
+                  <th>Product Name</th>
+                  <th>Product Category</th>
+                  <th>Product Price</th>
+                  <th>Description</th>
+                  <th>Edit</th>
+                </tr>
+              </thead>
+              <tbody role="rowgroups">
+                <tr>
+                  <td>1</td>
+                  <td>CPU</td>
+                  <td>Computers</td>
+                  <td>PHP 6,000</td>
+                  <td>Edi wow</td>
+                  <td style="text-align:center" role="gridcell  "><button class="btn btn-secondary ml-3 mt-3.5" onclick="location.href='#'"> <i class="fa fa-pencil"></i> &nbsp Edit</button></td>  
+                </tr>
+              </tbody>
+              </table>
+      </div>
+       <button class="btn btn-primary ml-3 mt-3.5" onclick="location.href='anproduct.php'">Add New</button>
+    </div>
+    </div>
+
+  <!-- Logout Modal-->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+            <button type="button" class="btn btn-primary">Logout</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="sticky-footer">
+      <div class="container">
+        <div class="text-center">
+          <small>COPYRIGHT © 2017 NEGO$YO PHILIPPINES. ALL RIGHT RESERVED</small>
+        </div>
+      </div>
+    </footer>
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Page level plugin JavaScript-->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+    <script src="vendor/datatables/jquery.dataTables.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin.min.js"></script>
+    <!-- Custom scripts for this page-->
+    <script src="js/sb-admin-datatables.min.js"></script>
+    <script src="js/sb-admin-charts.min.js"></script>
+</body>
+</html>
